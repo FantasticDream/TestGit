ba0653b1ab9523b4239fc1d7163d3b47cc1913b9
zc <2923289118@qq.com>
2020-09-11 01:38:28 +0800

    dev 2 commit

---
+++ src/main/java/com/zc/Test.java
@@ -0,0 +1,13 @@
+package com.zc;
+
+/*
+ *   @创建人  zc
+ *   @创建时间 2020/9/11 1:37
+ *   @包名: com.zc
+ *   @描述：
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("this is dev-2");
+    }
+}
